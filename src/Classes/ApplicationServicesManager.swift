34d58e050bf770e1676c046fd6e83575da07da35
Rafael Fernández <rafael.fernandez@sdos.es>
2019-12-16 15:56:53 +0100

    Support SDK iOS 13 SceneKit

--- src/Classes/ApplicationServicesManager.swift
+++ src/Classes/ApplicationServicesManager.swift
@@ -11,7 +11,7 @@ import CloudKit
 
 /// This is only a tagging protocol.
 /// It doesn't add more functionalities yet.
-public protocol ApplicationService: UIApplicationDelegate {}
+public protocol ApplicationService: UIApplicationDelegate, UIWindowSceneDelegate {}
 
 open class PluggableApplicationDelegate: UIResponder, UIApplicationDelegate {
     
@@ -406,6 +406,7 @@ open class PluggableApplicationDelegate: UIResponder, UIApplicationDelegate {
     public func application(_ application: UIApplication, viewControllerWithRestorationIdentifierPath identifierComponents: [String], coder: NSCoder) -> UIViewController? {
         for service in __services {
             if let viewController = service.application?(application, viewControllerWithRestorationIdentifierPath: identifierComponents, coder: coder) {
+                print("[SDOSPluggableApplicationDelegate] - Return first responder of \(#function)")
                 return viewController
             }
         }
@@ -509,5 +510,98 @@ open class PluggableApplicationDelegate: UIResponder, UIApplicationDelegate {
         for service in __services {
             service.application?(application, userDidAcceptCloudKitShareWith: cloudKitShareMetadata)
         }
+    }
+    
+    
+    //MARK: - SceneKit
+    
+    @available(iOS 13.0, *)
+    open func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        __services.forEach { $0.scene?(scene, willConnectTo: session, options: connectionOptions) }
+    }
+    
+    @available(iOS 13.0, *)
+    open func windowScene(_ windowScene: UIWindowScene, didUpdate previousCoordinateSpace: UICoordinateSpace, interfaceOrientation previousInterfaceOrientation: UIInterfaceOrientation, traitCollection previousTraitCollection: UITraitCollection) {
+        __services.forEach { $0.windowScene?(windowScene, didUpdate: previousCoordinateSpace, interfaceOrientation: previousInterfaceOrientation, traitCollection: previousTraitCollection) }
+    }
+
+    @available(iOS 13.0, *)
+    open func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        apply({ (service, completionHandler) -> Void in
+            service.windowScene?(windowScene, performActionFor: shortcutItem) { result in
+                completionHandler(result)
+            }
+        }, completionHandler: { results in
+            let result = results.reduce(false, { $0 || $1 })
+            completionHandler(result)
+        })
+    }
+
+    
+    @available(iOS 13.0, *)
+    open func windowScene(_ windowScene: UIWindowScene, userDidAcceptCloudKitShareWith cloudKitShareMetadata: CKShare.Metadata) {
+        __services.forEach { $0.windowScene?(windowScene, userDidAcceptCloudKitShareWith: cloudKitShareMetadata) }
+    }
+    
+    @available(iOS 13.0, *)
+    open func sceneDidDisconnect(_ scene: UIScene) {
+        __services.forEach { $0.sceneDidDisconnect?(scene) }
+    }
+    
+    @available(iOS 13.0, *)
+    open func sceneDidBecomeActive(_ scene: UIScene) {
+        __services.forEach { $0.sceneDidBecomeActive?(scene) }
+    }
+
+    @available(iOS 13.0, *)
+    open func sceneWillResignActive(_ scene: UIScene) {
+        __services.forEach { $0.sceneWillResignActive?(scene) }
+    }
+
+    @available(iOS 13.0, *)
+    open func sceneWillEnterForeground(_ scene: UIScene) {
+        __services.forEach {$0.sceneWillEnterForeground?(scene) }
+    }
+
+    @available(iOS 13.0, *)
+    open func sceneDidEnterBackground(_ scene: UIScene) {
+        __services.forEach { $0.sceneDidEnterBackground?(scene) }
+    }
+    
+
+    @available(iOS 13.0, *)
+    open func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        __services.forEach { $0.scene?(scene, openURLContexts: URLContexts) }
+    }
+    
+    @available(iOS 13.0, *)
+    open func stateRestorationActivity(for scene: UIScene) -> NSUserActivity? {
+        for service in __services {
+            if let result = service.stateRestorationActivity?(for: scene) {
+                print("[SDOSPluggableApplicationDelegate] - Return first responder of \(#function)")
+                return result
+            }
+        }
+        return nil
+    }
+
+    @available(iOS 13.0, *)
+    open func scene(_ scene: UIScene, willContinueUserActivityWithType userActivityType: String) {
+        __services.forEach { $0.scene?(scene, willContinueUserActivityWithType: userActivityType) }
+    }
+
+    @available(iOS 13.0, *)
+    open func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        __services.forEach { $0.scene?(scene, continue: userActivity) }
+    }
+
+    @available(iOS 13.0, *)
+    open func scene(_ scene: UIScene, didFailToContinueUserActivityWithType userActivityType: String, error: Error) {
+        __services.forEach { $0.scene?(scene, didFailToContinueUserActivityWithType: userActivityType, error: error) }
+    }
+
+    @available(iOS 13.0, *)
+    open func scene(_ scene: UIScene, didUpdate userActivity: NSUserActivity) {
+        __services.forEach { $0.scene?(scene, didUpdate: userActivity) }
     }
 }
